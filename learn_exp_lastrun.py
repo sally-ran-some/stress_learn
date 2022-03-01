9442a440f3168a60d0e8c38e0481b15cb486ea71
Sarah Marie Ransom-Laud <sarah.ransomlaud@austin.utexas.edu>
2022-02-28 21:14:07 -0600

    made syllable concatenator from bigger word one

--- learn_exp_lastrun.py
+++ learn_exp_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on Wed Jan 26 14:28:37 2022
+    on Wed Feb 23 19:26:34 2022
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
